6c1acb68764df00ade42b551f93368671c847f21
skxcv312 <skxcv322@google.com>
2025-05-13 23:34:06 +0900

    chore: GlobalExceptionHandler exception trace 추가
    - 예외를 보기 편하게함

--- src/main/java/org/zerock/study/global/GlobalExceptionHandler.java
+++ src/main/java/org/zerock/study/global/GlobalExceptionHandler.java
@@ -2,10 +2,13 @@ package org.zerock.study.global;
 
 
 import lombok.Builder;
+
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Log4j2
 @ControllerAdvice
 public class GlobalExceptionHandler {
     record ErrorDTO(
@@ -16,19 +19,21 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<?> illegalArgument(IllegalArgumentException e) {
-
+        log.error(e);
         return ResponseEntity.badRequest()
                 .body(new ErrorDTO("IllegalArgumentException", e.getMessage()));
     }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<?> runtimeException(RuntimeException e) {
+        log.error(e);
         return ResponseEntity.badRequest()
                 .body(new ErrorDTO("RuntimeException", e.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> exception(Exception e) {
+        log.error(e);
         return ResponseEntity.internalServerError()
                 .body(new ErrorDTO("Exception", e.getMessage()));
 
