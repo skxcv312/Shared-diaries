6a32fd292919980680479467535d1c2ca69cbd39
skxcv312 <skxcv322@google.com>
2025-05-21 19:43:20 +0900

    feat: 환경변수 관리를 위한 secrets.yml 파일 생성
    - JWT 비밀키를 하드코딩 대신 application.yml에서 주입받도록 변경
    - @Value 어노테이션을 활용한 외부 설정 분리

--- src/main/java/org/zerock/study/global/config/JwtConfig.java
+++ src/main/java/org/zerock/study/global/config/JwtConfig.java
@@ -10,12 +10,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @Getter
 public class JwtConfig {
+    @Value("${jwt.secret-key}")
+    String secret;
+
     private final Key secretKey;
+
     private final long accessTokenValidTime = 30 * 24 * 60 * 60 * 1000L; // 1h
     private final long refreshTokenValidTime = 24 * 60 * 60 * 1000L; //1 day
 
     public JwtConfig() {
-        String secret = "W91ci0zMi1ieXRlLXNlY3JldC1rZXktZm9yLUpXVDo1415fsdfhkwf1552";
         this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
     }
 
